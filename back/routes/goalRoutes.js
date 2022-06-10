3956ea1504e02259aa680d377a2fdd859517e8f4
iamayushy <ayushhuns@gmai.com>
2022-06-10 20:11:12 +0530

    api done

---
+++ back/routes/goalRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const {getGoals, 
+    setGoals, 
+    putGoals, 
+    deleteGoals
+    } = require('../controllers/goalController')
+
+// routes
+router.route('/', getGoals).post(setGoals)
+router.route('/:id').put(putGoals).delete(deleteGoals)
+
+
+module.exports = router
